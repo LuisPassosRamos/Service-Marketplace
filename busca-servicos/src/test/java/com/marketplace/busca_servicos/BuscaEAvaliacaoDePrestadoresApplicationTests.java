e8ff35e20a8562249871b36776c7b81dfe564305
Luis Guilherme Passos Ramos <129339010+LuisPassosRamos@users.noreply.github.com>
2024-12-16 01:00:24 -0300

    Add files via upload

---
+++ busca-servicos/src/test/java/com/marketplace/busca_servicos/BuscaEAvaliacaoDePrestadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.busca_servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaEAvaliacaoDePrestadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
